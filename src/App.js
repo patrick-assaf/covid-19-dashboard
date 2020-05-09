2823bb194003afa94d1ffc6d56b7c68d846a73de
Patrick Assaf <patrickassaf96@gmail.com>
2020-05-09 01:05:49 -0400

    Initialize components JSX files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { Cards, Chart, CountrySelector } from './components';
 
 class App extends Component {
 
     render() {
         return(
             <div>
-                <h1>
-                    COVID-19 Tracker App
-                </h1>
+                <Cards />
+                <CountrySelector />
+                <Chart />
             </div>
         )
     }
