ec8f951c37f1c11603962518a43180bd3511ac6d
Patrick Assaf <patrickassaf96@gmail.com>
2020-05-09 01:24:19 -0400

    Fetch data from api through axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Cards, Chart, CountrySelector } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api'
 
 class App extends Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
 
     render() {
         return(
