52d3a45f2f27006fb1340d93a16fc7981ada512a
Patrick Assaf <patrickassaf96@gmail.com>
2020-05-11 16:17:18 -0400

    Implement Card JSX structure inside Card Component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
 
 const Cards = (props) => {
     console.log(props.data)
 
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center" >
+                <Grid item component={Card} >
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom >Infected</Typography>
+                        <Typography variant="h5" >((PRINT DATA))</Typography>
+                        <Typography color="textSecondary" >((PRINT DATE))</Typography>
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
